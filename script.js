d2e3c00e34a7d821b7d0d6734bf48b9bc1dd2b47
Austin Sorrell <austin.sorrell@student.cart.org>
2022-05-10 10:28:17 -0700

    Header

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("mySidemenu").style.width = "250px";
+  }
+  
+  function closeMenu() {
+    document.getElementById("mySidemenu").style.width = "0";
+  }
