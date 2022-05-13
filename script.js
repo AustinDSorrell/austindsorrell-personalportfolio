aa1bc3e3a8868aa6c430b09deae9ec6f0e2e604c
Austin Sorrell <austin.sorrell@student.cart.org>
2022-05-13 10:07:15 -0700

    Content Update

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuProperty = document.querySelector('.menuButton');
-const hero = document.querySelector('.hero');
-
-menuProperty.addEventListener('click', () => {
-  menuProperty.classList.mainMenu('active');
-  hero.classList.mainMenu('active');
-})
